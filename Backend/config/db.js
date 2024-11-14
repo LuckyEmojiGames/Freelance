8c1bb5cf6845340d2925dddbe005fc6e1a027885
Adonis Joseph <magic.front.68@gmail.com>
2024-11-14 11:07:26 -0800

    backend init

---
+++ Backend/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const connectDb = () => {
+  mongoose
+    .connect(process.env.SKILLCOUP_DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(console.log("Db Is Connected"))
+    .catch((err) => {
+      console.log("Db Connection issue");
+      console.log("====================================");
+      console.log(err);
+      console.log("====================================");
+    });
+};
+
+module.exports = connectDb;
