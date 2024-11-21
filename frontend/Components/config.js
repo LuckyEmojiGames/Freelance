aebc8736eb3ba0e9090fdf8c946fdf1508333a49
Adonis Joseph <magic.front.68@gmail.com>
2024-11-21 13:56:18 -0800

    repair config

--- frontend/Components/config.js
+++ frontend/Components/config.js
@@ -1,3 +1,1 @@
-// export const BASE_URL='https://skillcoupbackend-production.up.railway.app/api/v1';
-export const BASE_URL = 'http://172.16.5.154:4000/api/v1';
-export const PAYMENT_BASE_URL = 'https://skillcoup-api.onrender.com';
+export const BASE_URL = 'http://localhost:4000/api/v1';
